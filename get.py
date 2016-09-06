830dcc5b7f75894cc577cdb95f6f30e53c7ae9e4
Jason Au <jsonau@gmail.com>
2016-09-06 18:07:42 -0400

    Refactor by looping

--- get.py
+++ get.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import urllib2
 import json
 
@@ -8,13 +7,35 @@ def getJSON(url):
 		urllib2.urlopen(url).read()
 	)
 
-template = " {}: {}"
+# iterate a dictionary given a ordered list of keys and return the result
+def chain_get(dictionary, ordered_keys_to_iterate):
+	value = dictionary
+	for key in ordered_keys_to_iterate:
+		value = value.get(key)
+	return value
 
-data = getJSON("https://coinbase.com/api/v1/prices/buy")
-print template.format( 'Coinbase', data['amount'])
 
-data = getJSON("https://www.bitstamp.net/api/ticker/")
-print template.format( 'BitStamp', data['last'])
+API = [
+	{
+		'title': 'Coinbase',
+		'url': 'https://coinbase.com/api/v1/prices/buy',
+		'nodes_to_price': ['amount']
+	},
+	{
+		'title': 'BitStamp',
+		'url': 'https://www.bitstamp.net/api/ticker/',
+		'nodes_to_price': ['last']
+	},
+	{
+		'title': 'BTC-e',
+		'url': 'https://btc-e.com/api/2/btc_usd/ticker',
+		'nodes_to_price': ['ticker', 'last']
+	},
+]
 
-data = getJSON("https://btc-e.com/api/2/btc_usd/ticker")
-print template.format( "BTC-e", data['ticker']['last'])
+RESULT_TEMPLATE = " {}: {}"
+
+
+for api in API:
+	data = getJSON(api['url'])
+	print RESULT_TEMPLATE.format(api['title'], chain_get(data, api['nodes_to_price']))
